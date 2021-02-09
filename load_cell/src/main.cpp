b9abdd62dd7422253c0d79db0cb1cc99060e22da
marti456 <martidami5@gmail.com>
2021-02-09 22:17:59 +0200

    Made base functionality

--- load_cell/src/main.cpp
+++ load_cell/src/main.cpp
@@ -1,14 +1,53 @@
 #include "HX711.h"
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
+float SCALE_CALIBRATE = 2280.f;
+float weight, cup_weight; 
+float threshold = 10; //the border when we accept there is a cup on the load cell
 
+const int WATER_PUMP_1 = 13;
+const int WATER_PUMP_2 = 12;
+const int WATER_PUMP_3 = 11;
+
+const int BUTTON_1 = 10;
+const int BUTTON_2 = 9;
+const int BUTTON_3 = 8;
 
 HX711 scale;
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("HX711 Demo");
+int check_button(){
+  while(1){
 
+    int is_clicked_1 = digitalRead(BUTTON_1);
+    delay(10);
+    int is_clicked_2 = digitalRead(BUTTON_2);
+    delay(10);
+    int is_clicked_3 = digitalRead(BUTTON_3);
+
+
+    if(is_clicked_1 == HIGH){
+      return 1;
+    }
+    else if(is_clicked_2 == HIGH){
+      return 2;
+    }
+    else if(is_clicked_3 == HIGH){
+      return 3;
+    }
+
+  }
+}
+
+void setup() {
+  pinMode(WATER_PUMP_1, OUTPUT);
+  pinMode(WATER_PUMP_2, OUTPUT);
+  pinMode(WATER_PUMP_3, OUTPUT);
+
+  pinMode(BUTTON_1, INPUT);
+  pinMode(BUTTON_2, INPUT);
+  pinMode(BUTTON_3, INPUT);
+
+  Serial.begin(9600);
   Serial.println("Initializing the scale");
 
   // Initialize library with data output pin, clock input pin and gain factor.
@@ -19,24 +58,8 @@ void setup() {
   // default "128" (Channel A) is used here.
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());			// print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
-						// by the SCALE parameter (not set yet)
-
-  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.set_scale(SCALE_CALIBRATE);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();				        // reset the scale to 0
-
-  Serial.println("After setting up the scale:");
 
   Serial.print("read: \t\t");
   Serial.println(scale.read());                 // print a raw reading from the ADC
@@ -54,12 +77,41 @@ void setup() {
   Serial.println("Readings:");
 }
 
+int PUMP;
 void loop() {
+  int read_button = check_button();
+  switch(read_button){
+    case 1:
+      PUMP = WATER_PUMP_1;
+      break;
+    
+    case 2:
+      PUMP = WATER_PUMP_2;
+      break;
+    
+    case 3:
+      PUMP = WATER_PUMP_3;
+      break;
+  }
+
   Serial.print("one reading:\t");
   Serial.print(scale.get_units(), 1);
   Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(10), 1);
+  weight = scale.get_units(10);
+  Serial.println(weight, 1);
 
-  delay(1000);
+  if(weight > threshold){
+    cup_weight = weight;
+    while(weight < cup_weight + 50){
+      
+      digitalWrite(PUMP, HIGH);
+      weight = scale.get_units();
+    }
+
+    digitalWrite(PUMP, LOW);
+
+  }
+
+  delay(10);
 
 }
